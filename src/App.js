124cc31a8dbf123f4bd28b0d15c51e233ad7b3e1
vchrpng <vchrpng@gmail.com>
2017-08-28 22:30:40 +0700

    Click on character key log on console

--- src/App.js
+++ src/App.js
@@ -3,31 +3,39 @@ import {MainContainer,Keyboard,KeyButton,Row,List,Title} from './App.style'
 import {Icon , Input , Divider} from 'semantic-ui-react'
 
 export default class App extends Component {
-  render() {
+  constructor(props){
+    super(props)  
+    this.state = {value:""}
+  }
 
-    
-    function selectedChar(input){
-   
-      console.log(input)
+   handleClick(char)  {
+    console.log(char)
+    }
+    handleChange(e){
+      this.setState({value:e.target.value})
     }
 
+
+  render() {
 
     const keys =  [[1,2,3,4,5,6,7,8,9,0],
                   ['q','w','e','r','t','y','u','i','o','p'],
                   ['a','s','d','f','g','h','j','k','l'],
                   ['z','x','c','v','b','n','m','_']]
 
-    const firstRow = keys[0].map((char)=>
-            <KeyButton value={char} key={char.toString()}>{char}</KeyButton>)
+    const firstRow = keys[0].map((char,index)=>
+            <KeyButton key={index} onClick={this.handleClick.bind(this,char)}>{char}</KeyButton>)
 
-    const secondRow = keys[1].map((key)=>
-            <KeyButton>{key}</KeyButton>)
+    const secondRow = keys[1].map((char,index)=>
+            <KeyButton key={index} onClick={this.handleClick.bind(this,char)}>{char}</KeyButton>)
 
-    const thirdRow = keys[2].map((key)=>
-            <KeyButton>{key}</KeyButton>)
+    const thirdRow = keys[2].map((char,index)=>
+            <KeyButton key={index} onClick={this.handleClick.bind(this,char)}>{char}</KeyButton>)
 
-    const fourthRow = keys[3].map((key)=>
-            <KeyButton>{key}</KeyButton>)
+    const fourthRow = keys[3].map((char,index)=>
+            <KeyButton key={index} onClick={this.handleClick.bind(this,char)}>{char}</KeyButton>)
+
+    
 
     return (
       <div>
@@ -36,7 +44,8 @@ export default class App extends Component {
             <Title>ENTER YOUR TWITTER HERE</Title>
             <p>Use your mouse (or tap the keyboard)... You'll be glad you did ! </p>
           
-                <Input iconPosition='left' placeholder='Email'>
+                <Input onChange={this.handleChange.bind(this)} value={this.state.value} 
+                      iconPosition='left' placeholder='Email'>
                       <Icon name='at' />
                       <input />
                 </Input>
@@ -48,7 +57,7 @@ export default class App extends Component {
                 <Row>
                   <List>
                    {firstRow}
-                   <KeyButton onClick={selectedChar('omg')}>delete</KeyButton>
+                   <KeyButton>delete</KeyButton>
                   </List>
                 </Row>
 
